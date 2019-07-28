f05d241e5ad73bc135d1ea8c9c2ab563e67c8c46
czoido <mrgalleta@gmail.com>
2019-07-28 16:07:20 +0200

    minor changes

--- scripts/check_deterministic.py
+++ scripts/check_deterministic.py
@@ -35,7 +35,7 @@ def get_binary_names(console_txt):
     bin_files = []
     package_folder = ""
     for line in console_txt.splitlines():
-        print(line)
+        #print(line)
         binary_extensions = [".lib", ".exe", ".dll", ".a", ".so", ".dylib"]
         line = str(line)
         if any(extension in line for extension in binary_extensions):
